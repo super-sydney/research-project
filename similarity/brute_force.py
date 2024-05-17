0938face658e311913a5a2de7d621502a4710a68
super-sydney <s.kho-1@student.tudelft.nl>
2024-05-17 11:26:34 +0200

    Use ratio test for SIFT and ORB

--- similarity/brute_force.py
+++ similarity/brute_force.py
@@ -11,4 +11,11 @@ class BruteForce(Similarity):
     def compare(self, d1, d2):
         bf = cv2.BFMatcher()
         matches = bf.knnMatch(d1, d2, k=2)
-        return 1 / len(matches)
+
+        # Apply ratio test
+        good_matches = []
+        for m, n in matches:
+            if m.distance < 0.75 * n.distance:
+                good_matches.append([m])
+
+        return len(good_matches)
