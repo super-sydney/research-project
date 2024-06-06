3e09f3435ee40a4fbadffc3036b21c670767da4b
super-sydney <s.kho-1@student.tudelft.nl>
2024-06-06 16:25:42 +0200

    Modify main to run multiple models

--- main.py
+++ main.py
@@ -9,12 +9,14 @@ logger = logging.getLogger(__name__)
 
 if __name__ == "__main__":
     logger.info("Starting the evaluation")
-    m = Chebyshev()
+    ms = [Zernike(), Legendre(), Chebyshev(), BesselFourier()]
     c = Euclidean()
 
-    e = Evaluation(m, c, "dataset/training_subset1/mini")
-    score = e.evaluate(visualize=False, save_db=False)
-    best_possible_score = e.best_possible_score()
+    for m in ms:
+        e = Evaluation(m, c, "dataset/training_subset/base")
+        score, res = e.evaluate(visualize=False, save_db=False)
+        best_possible_score = e.best_possible_score()
 
-    logging.debug(f"Score: {score}, Best possible score: {best_possible_score}")
-    logging.info(f'Ran feature extraction {m} with comparison {c} and got score {score / best_possible_score}')
+        res.to_csv(f'evaluation/results/{m}24.csv')
+        # logging.debug(f"Score: {score}, Best possible score: {best_possible_score}")
+        # logging.info(f'Ran feature extraction {m} with comparison {c} and got score {score / best_possible_score}')
